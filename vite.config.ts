4ce94c7e9fcf3db9359b7b80a007afb9ad32fef1
Kamil <kamil.naskret.dev@gmail.com>
2026-02-27 16:56:19 +0100

    feat: vitest
    Fully setup vitest and all testing dependencies.
    Configuration and add a simple test for the button component.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
@@ -10,5 +11,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.ts",
   },
 });
